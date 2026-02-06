a64253228e3640471e515036553ddcb20432811d
Azamat-akoooo <tojiddinfayzullaev981@gmail.com>
2026-02-06 08:52:31 +0500

    fixed

--- app/Filament/Resources/Meets/Pages/CreateMeet.php
+++ app/Filament/Resources/Meets/Pages/CreateMeet.php
@@ -8,4 +8,11 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateMeet extends CreateRecord
 {
     protected static string $resource = MeetResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['user_id'] = auth()->id();
+        
+        return $data;
+    }
 }
