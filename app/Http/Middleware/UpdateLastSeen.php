a3f0dc1dc9c8a020f00376bbcda9643858e9db09
Azamat-akoooo <tojiddinfayzullaev981@gmail.com>
2026-02-08 10:25:52 +0500

    added middleware

---
+++ app/Http/Middleware/UpdateLastSeen.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class UpdateLastSeen
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->user()) {
+            $request->user()->update(['last_seen_at' => now()]);
+        }
+
+        return $next($request);
+    }
+}
