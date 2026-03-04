7cc6648705c57582919a459142a6e1e0a68251a2
Azamat-akoooo <tojiddinfayzullaev981@gmail.com>
2026-03-05 04:51:20 +0500

    fixed

--- app/Filament/Resources/Workers/Pages/EditWorker.php
+++ app/Filament/Resources/Workers/Pages/EditWorker.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Log;
 use Kreait\Firebase\Exception\Messaging\NotFound;
 use Kreait\Firebase\Exception\Messaging\InvalidMessage;
 use Kreait\Firebase\Messaging\CloudMessage;
-use Kreait\Firebase\Messaging\Notification as FirebaseNotification;
+
 
 class EditWorker extends EditRecord
 {
@@ -42,10 +42,6 @@ class EditWorker extends EditRecord
 
                     try {
                         $message = CloudMessage::withTarget('token', $user->fcm)
-                            ->withNotification(FirebaseNotification::create(
-                                'GPS so\'rov',
-                                'Joylashuvingiz so\'ralmoqda...'
-                            ))
                             ->withData(['type' => 'location_request'])
                             ->withHighestPossiblePriority();
 
